222d7a1c9357427e79f6ccbe61a1cafa97c3b2b9
Daniyal-ali-code <daniyalaliwork24@gmail.com>
2025-01-20 23:32:59 +0500

    Daniyal

--- First Semester/PROGRAMMING FUNDAMENTALS THEORY/Assignment # 02/Assignment # 02 - (Question # 09).cpp
+++ First Semester/PROGRAMMING FUNDAMENTALS THEORY/Assignment # 02/Assignment # 02 - (Question # 09).cpp
@@ -1,42 +1,37 @@
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
 
-void insertAndSortDescending(int arr[], int& n, int newValue) {
-    // Insert the new value at the end of the array
-    arr[n] = newValue;
-    n++;
-
-    // Sort the array in descending order using std::sort with a custom comparator
-    sort(arr, arr + n, greater<int>());
+void func( int arr[], int &n, int newVal){
+	arr[n] = newVal;
+	n++;
+	
+	sort( arr, arr + n, greater<int>() );
 }
 
-int main() {
-    int arr[100]; // Assuming the array size will not exceed 100
-    int n;
-
-    cout << "Input the number of elements to be stored in the array: ";
-    cin >> n;
-
-    cout << "Input " << n << " elements in the array:" << endl;
-    for (int i = 0; i < n; i++) {
-        cout << "element - " << i + 1 << " : ";
-        cin >> arr[i];
-    }
-
-    int newValue;
-    cout << "Input the value to be inserted: ";
-    cin >> newValue;
-
-    insertAndSortDescending(arr, n, newValue);
-
-    cout << "The array after insertion and sorting in descending order is:" << endl;
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-
-    return 0;
+int main (){
+	int arr[500];
+	int n;
+	
+	cout << "Enter number of values you want to store in the array: ";
+	cin >> n;
+	
+	for( int i = 0; i < n; i++){
+		cout << "Enter element - " << i + 1 << " : ";
+		cin >> arr[i];
+	}
+	
+	int newVal;
+	cout << "Enter the new element: "<< endl;
+	cin >> newVal;
+	
+	func( arr, n, newVal);
+	
+	cout << "The array after sorting in descending order is: ";
+	for (int i = 0; i < n; i++){
+		cout << arr[i] <<endl;
+	}
+	cout << endl;
+	
+	return 0;
 }
-
